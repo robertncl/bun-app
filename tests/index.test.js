1ab8402e96646f36f7d8844e4f76dd663d943974
Robert Ngo <robertncl@gmail.com>
2025-04-02 11:10:07 +0800

    fix test

--- tests/index.test.js
+++ tests/index.test.js
@@ -12,7 +12,7 @@ describe('API Routes', () => {
         const req = new Request('http://localhost/');
         const response = await controller.getHome(req);
         const text = await response.text();
-        expect(text).toBe('Welcome to the Home Page');
+        expect(text).toBe('This is a freshly steamed bun');
     });
 
     test('GET /data should return data', async () => {
